a118cd092debf60c33c01150f1e2faaa358c0cea
deskcodeFun <panya.korkusol@gmail.com>
2025-05-22 16:11:54 +0700

    fix position impage center in Homveview"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,12 @@
 <template>
   <!-- create BaseCard to view all computer -->
   <!-- <div class="bg-amber-200 grid grid-cols-4"> -->
-  <div class="bg-amber-200 w-screen my-8 flex flex-row flex-wrap justify-center">
-    <div class="bg-green-800 w-fit" v-for="user in users" key:user.id>
+  <div class="w-screen my-8 flex flex-row flex-wrap justify-center">
+    <div class="w-fit" v-for="user in users" key:user.id>
       <div class="bg-white border-2 border-gray-200 rounded-2xl w-xl mx-4 my-4 py-4 px-4">
+        <img class="mx-auto mb-4" src="\src\assets\images\hp1.png" alt="computer image" />
         <table>
           <tbody>
-            <tr>
-              <img src="\src\assets\images\hp1.png" alt="computer image" width="320" />
-            </tr>
             <tr>
               <th class="text-gray-400 text-left">User Name</th>
               <td class="text-blue-800 px-4">{{ user.user_name }}</td>
