ef81a1570a4923cd56900c8e15f0f62602d5eaec
deskcodeFun <panya.korkusol@gmail.com>
2025-05-24 11:47:53 +0700

    add user name on computer state:onUse and add color for use and free state

--- src/views/ComputerView.vue
+++ src/views/ComputerView.vue
@@ -10,22 +10,29 @@
   </div>
   <div class="w-screen my-4 mx-24 flex flex-row flex-wrap justify-start">
     <div class="w-fit" v-for="computer in computers" key:computer.id>
-      <div class="grid grid-cols-2 w-xl p-4 m-2 bg-white border-2 border-gray-200 rounded-2xl">
+      <div class="flex flex-row p-4 m-2 bg-white border-2 border-gray-200 rounded-2xl">
         <!-- show pictuer on left -->
-        <img class="" src="\src\assets\images\hp1.png" alt="computer image" />
+        <img class="pr-8 w-fit" src="\src\assets\images\hp1.png" alt="computer image" />
 
         <table>
           <tbody>
+            <tr>
+              <th class="text-gray-400 text-left align-baseline">Status</th>
+              <td class="px-4 text-red-800">
+                <p v-if="computer.status">{{ computer.staff[0].user_name }}</p>
+                <p v-else class="text-green-800 font-bold">Free</p>
+              </td>
+            </tr>
             <tr>
               <th class="text-gray-400 text-left">Brand</th>
               <td class="text-blue-800 px-4">{{ computer.brand }}</td>
             </tr>
             <tr>
               <th class="text-gray-400 text-left align-baseline">CPU</th>
-              <td class="text-blue-800 px-4 w-md text-warp">{{ computer.cpu }}</td>
+              <td class="text-blue-800 px-4 w-sm text-warp">{{ computer.cpu }}</td>
             </tr>
             <tr>
-              <th class="text-gray-400 text-left">Ram</th>
+              <th class="text-gray-400 text-left align-baseline">Ram</th>
               <td class="text-blue-800 px-4">{{ computer.ram }}</td>
             </tr>
             <tr>
@@ -35,12 +42,6 @@
             <tr>
               <th class="text-gray-400 text-left align-baseline">Serial Tag</th>
               <td class="text-blue-800 px-4">{{ computer.serial_tag }}</td>
-            </tr>
-            <tr>
-              <th class="text-gray-400 text-left align-baseline">Status</th>
-              <td class="text-blue-800 px-4">
-                {{ computer.status ? 'on use' : 'Free' }}
-              </td>
             </tr>
           </tbody>
         </table>
@@ -55,41 +56,30 @@ import { ref, onMounted } from 'vue'
 import { supabase } from '@/lib/supabaseClient.js'
 
 const computers = ref([])
-// const message = ref('')
-// function filter(message) {
-//   if (message.value === all) {
-//     getComputers()
-//     console.log('click on all', message)
-//     return
-//   } else if (message.value === use) {
-//     getUseComputer()
-//     console.log('click on Use', message)
-//     return
-//   } else {
-//     getFreeComputer()
-//     console.log('click on Stock', message)
-//     return
-//   }
-// }
 
 async function getComputers() {
   // const { data, error } = await supabase.from('computer').select().is('status', 'false')
 
-  const { data, error } = await supabase.from('computer').select().order('id')
+  const { data, error } = await supabase.from('computer').select(`*, staff(user_name)`).order('id')
   console.log(data)
   computers.value = data
 }
 async function getUseComputer() {
-  const { data, error } = await supabase.from('computer').select().is('status', 'true').order('id')
-
-  //const { data, error } = await supabase.from('computer').select()
+  const { data, error } = await supabase
+    .from('computer')
+    .select(`*, staff(user_name)`)
+    .is('status', 'true')
+    .order('id')
   console.log(data)
   computers.value = data
 }
 async function getFreeComputer() {
-  const { data, error } = await supabase.from('computer').select().is('status', 'false').order('id')
-
-  //const { data, error } = await supabase.from('computer').select()
+  const { data, error } = await supabase
+    .from('computer')
+    .select(`*, staff(user_name)`)
+    .is('status', 'false')
+    .order('id')
+  console.log(data)
   computers.value = data
 }
 
