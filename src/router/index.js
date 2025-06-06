fef076d0326011124eebad5ed750b8e9213a347d
deskcodeFun <panya.korkusol@gmail.com>
2025-06-06 08:13:27 +0700

    Changes to be committed: 	modified:   src/router/index.js 	new file:   src/views/NotFoundView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import FormView from '../views/FormView.vue'
 import DetailView from '../views/DetailView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: DetailView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 })
