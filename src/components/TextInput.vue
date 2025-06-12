7f0b1b56de11b857ad25f2278f688ff188261c7e
deskcodeFun <panya.korkusol@gmail.com>
2025-06-12 15:57:59 +0700

    add New computer into database

---
+++ src/components/TextInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p class="mr-4 mt-4 text-gray-600">{{ props.label }}</p>
+    <input
+      class="w-full border-b-1 nowrap rounded p-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      :type="props.type"
+      :placeholder="props.placeholder"
+      :value="props.modelValue"
+      @input="handleInput($event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  label: {
+    type: String,
+    delfault: '',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+const handleInput = (newValue) => {
+  emit('update:modelValue', newValue)
+}
+</script>
--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,5 +1,307 @@
 <template>
-  <P>This is form to update edit computer</P>
+  <!-- check login with auth user before add,update,delete computer -->
+  <!-- DON'T forget to update computer-update table -->
+  <p class="my-4 mx-4 text-2xl font-bold text-blue-900">Add New Computer</p>
+  <div class="flex flex-row">
+    <!-- Form: Add new computer we don't need validation data cause it's all text -->
+    <div class="w-1/2 pr-24 pl-4">
+      <p>Computer Specification</p>
+      <!-- <form @submit.prevent="addSubmit">
+        <TextInput
+          v-model.lazy.trim="assetModel"
+          label="Asset"
+          placeholder="Asset tag from accounting"
+        ></TextInput>
+      </form> -->
+      <form @submit.prevent="addSubmit">
+        <TextInput
+          v-model.lazy.trim="assetComponent.modelValue"
+          :label="assetComponent.label"
+          :placeholder="assetComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="serialComponent.modelValue"
+          :label="serialComponent.label"
+          :placeholder="serialComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="brandComponent.modelValue"
+          :label="brandComponent.label"
+          :placeholder="brandComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="modelComponent.modelValue"
+          :label="modelComponent.label"
+          :placeholder="modelComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="cpuComponent.modelValue"
+          :label="cpuComponent.label"
+          :placeholder="cpuComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="aiComponent.modelValue"
+          :label="aiComponent.label"
+          :placeholder="aiComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="graphicComponent.modelValue"
+          :label="graphicComponent.label"
+          :placeholder="graphicComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="screenComponent.modelValue"
+          :label="screenComponent.label"
+          :placeholder="screenComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="ramComponent.modelValue"
+          :label="ramComponent.label"
+          :placeholder="ramComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="maxRamComponent.modelValue"
+          :label="maxRamComponent.label"
+          :placeholder="maxRamComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="harddiskComponent.modelValue"
+          :label="harddiskComponent.label"
+          :placeholder="harddiskComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="harddiskSlotComponent.modelValue"
+          :label="harddiskSlotComponent.label"
+          :placeholder="harddiskSlotComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="lanPortComponent.modelValue"
+          :label="lanPortComponent.label"
+          :placeholder="lanPortComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="wirelessSlotComponent.modelValue"
+          :label="wirelessSlotComponent.label"
+          :placeholder="wirelessSlotComponent.placeholder"
+        ></TextInput>
+        <TextInput
+          v-model.lazy.trim="bluetoothComponent.modelValue"
+          :label="bluetoothComponent.label"
+          :placeholder="bluetoothComponent.placeholder"
+        ></TextInput>
+      </form>
+
+      <div>
+        <button
+          class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+          @click="addSubmit"
+        >
+          Add Computer
+        </button>
+      </div>
+    </div>
+    <!-- Add image form: select picture and resize to resolution H:150px -->
+    <div>
+      <p class="">Add imgage</p>
+    </div>
+  </div>
+
+  <!-- Form: Add new user -->
+  <!-- Form: update computer -->
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import TextInput from '@/components/TextInput.vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const form = reactive({
+  asset_tag: '',
+  serial_tag: '',
+  brand: '',
+  model: '',
+  cpu: '',
+  ai: '',
+  graphic: '',
+  screen_size: '',
+  ram: '',
+  max_ram: '',
+  harddisk: '',
+  harddisk_slot: '',
+  lan_port: '',
+  wireless: '',
+  bluetooth: '',
+})
+const assetComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'asset tag from accounting',
+  label: 'Asset',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const serialComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'computer serial number',
+  label: 'Serial',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const brandComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'computer brand name',
+  label: 'Brand',
+  errorMessages: null,
+  pattern: '',
+  minLength: 2,
+  maxLength: 50,
+})
+const modelComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'model of computer',
+  label: 'Model',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const cpuComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'cpu generation and model',
+  label: 'CPU',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const aiComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'type of ai processor',
+  label: 'AI',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const graphicComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'type of graphic',
+  label: 'Graphic',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const screenComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'display size',
+  label: 'Display',
+  errorMessages: null,
+  pattern: '',
+  minLength: 5,
+  maxLength: 50,
+})
+const ramComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'capacity ram',
+  label: 'Ram',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const maxRamComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'maximum upgrade ram',
+  label: 'Max Ram',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const harddiskComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'capacity of hard disk',
+  label: 'Hard disk',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const harddiskSlotComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'upgrade slot hard disk',
+  label: 'HD slot',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const lanPortComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'LAN speed',
+  label: 'LAN',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const wirelessSlotComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'wireless type',
+  label: 'Wireless',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+const bluetoothComponent = reactive({
+  modelValue: '',
+  type: 'text',
+  placeholder: 'blutetooth version',
+  label: 'Bluetooth',
+  errorMessages: null,
+  pattern: '',
+  minLength: 1,
+  maxLength: 50,
+})
+
+const addSubmit = async () => {
+  const newComputer = {
+    asset_tag: assetComponent.modelValue,
+    serial_tag: serialComponent.modelValue,
+    brand: brandComponent.modelValue,
+    model: modelComponent.modelValue,
+    cpu: cpuComponent.modelValue,
+    ai: aiComponent.modelValue,
+    graphic: graphicComponent.modelValue,
+    screen_size: screenComponent.modelValue,
+    ram: ramComponent.modelValue,
+    max_ram: maxRamComponent.modelValue,
+    harddisk: harddiskComponent.modelValue,
+    harddisk_slot: harddiskSlotComponent.modelValue,
+    lan_port: lanPortComponent.modelValue,
+    wireless: wirelessSlotComponent.modelValue,
+    bluetooth: bluetoothComponent.modelValue,
+  }
+  const { error } = await supabase.from('computer').insert(newComputer)
+  console.log('erroris :', error)
+}
+</script>
