f930b75d2b5c75ac5522dcbbd04a12a571c0fa33
deskcodeFun <panya.korkusol@gmail.com>
2025-07-03 13:48:04 +0700

    modified:   src/components/ComputerTotalCard.vue 	modified:   src/components/TheTopbar.vue make response

--- src/components/TheTopbar.vue
+++ src/components/TheTopbar.vue
@@ -3,18 +3,32 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex flex-row rounded-2xl shadow-md md:p-2">
-    <p class="font-bold text-blue-900 text-shadow-lg text-2xl">Computer Stock</p>
+  <div class="w-fit flex flex-col md:flex-row p-2 md:p-4">
+    <p class="font-bold text-sm text-center text-blue-900 text-shadow-lg md:text-2xl">
+      Computer Stock
+    </p>
+
     <!-- Menu in topbar, create menu-item, Navbar-->
-    <nav class="text-lg px-8 py-1 hover:pointer">
-      <RouterLink to="/" class="px-4" active-class="bg-sky-200 px-4 py-2 rounded-xl"
+    <nav class="px-2 md:px-8 hover:pointer">
+      <RouterLink
+        to="/"
+        class="px-4 text-sm md:text-lg"
+        active-class="bg-sky-200 px-2 md:px-4 py-1 md:py-2 rounded-xl"
         >Home</RouterLink
       >
-      <RouterLink to="/services" class="px-4" active-class="bg-sky-200 px-4 py-2 rounded-xl">
+      <RouterLink
+        to="/services"
+        class="px-4 text-sm md:text-lg"
+        active-class="bg-sky-200 px-4 py-2 rounded-xl"
+      >
         IT-Services
       </RouterLink>
-      <RouterLink to="/about" class="px-4" active-class="bg-sky-200 px-4 py-2 rounded-xl">
-        about
+      <RouterLink
+        to="/about"
+        class="px-4 text-sm md:text-lg"
+        active-class="bg-sky-200 px-4 py-2 rounded-xl"
+      >
+        About
       </RouterLink>
     </nav>
   </div>
